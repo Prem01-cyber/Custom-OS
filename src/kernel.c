9c63b662bd8e92b9970a45daa1b39fa6be45511f
Prem01-cyber <jampuramprem01@gmail.com>
2024-09-13 18:50:33 +0530

    added interrupt descriptor table for the protected mode

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,74 @@
 #include "kernel.h"
+#include <stdint.h>
+#include <stddef.h>
+#include "idt/idt.h"
+
+
+uint16_t *video_mem = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_column = 0;
+
+uint16_t terminal_make_char(char c, char colour)
+{
+    return (colour << 8) | c;
+}
+
+void terminal_putchar(int x, int y, char c, char colour)
+{
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, colour);
+}
+
+void terminal_writechar(char c, char colour)
+{
+    if (c == '\n' || terminal_row >= VGA_HEIGHT)
+    {
+        terminal_row++;
+        terminal_column = 0;
+        return;
+    }
+    terminal_putchar(terminal_column, terminal_row, c, colour);
+    terminal_column++;
+    if (terminal_column >= VGA_WIDTH)
+    {
+        terminal_column = 0;
+        terminal_row++;
+    }
+}
+
+void terminal_initialize()
+{
+    video_mem = (uint16_t *)0xB8000;
+    for (int y = 0; y < VGA_HEIGHT; y++)
+    {
+        for (int x = 0; x < VGA_WIDTH; x++)
+        {
+            terminal_putchar(x, y, ' ', 2);
+        }
+    }
+}
+
+size_t strlen(const char *str)
+{
+    size_t len = 0;
+    while (str[len])
+    {
+        len++;
+    }
+    return len;
+}
+
+void print(const char *str)
+{
+    size_t len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        terminal_writechar(str[i], 2);
+    }
+}
 
 void kernel_main()
 {
-
+    terminal_initialize();
+    print("Hello, World!\nWoowww");
+    idt_init();
 }
