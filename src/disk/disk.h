df967e3fb81fb3812ec4ca4bdfc74259ff77e704
Prem01-cyber <jampuramprem01@gmail.com>
2024-09-14 15:03:50 +0530

    Added file manager for file system like creating file descriptors and related stuff

--- src/disk/disk.h
+++ src/disk/disk.h
@@ -2,6 +2,7 @@
 #define DISK_H
 
 #include <stdint.h>
+#include "fs/file.h"
 
 typedef unsigned int PEACHOS_DISK_TYPE;
 
@@ -11,6 +12,8 @@ struct disk
 {
     PEACHOS_DISK_TYPE type;
     int sector_size;
+
+    struct filesystem *filesystem;
 };
 
 void disk_search_and_init();
